88bc8d352122dbc99e0ebbf7e53bf57df785e306
CoderQCFromScraatch <73763394+CoderQCFromScraatch@users.noreply.github.com>
2021-01-31 11:15:35 -0800

    Update RConHost.bat

--- RConHost.bat
+++ RConHost.bat
@@ -1,13 +1,20 @@
 @echo off
+cd C:\RMP\rycmd\RubCmSettings
+call ini /s ConsoleHost /i Prompt config.ini > ini.tmp
+for /f "delims=" %%x in (ini.tmp) do set prmpt=%%x
+call ini /s ConsoleHost /i CmdDir config.ini > ini.tmp
+for /f "delims=" %%x in (ini.tmp) do set cmddir=%%x
+call ini /s ConsoleHost /i Startup config.ini > ini.tmp
+for /f "delims=" %%x in (ini.tmp) do set startup=%%x
 call ini /s ConsoleHost /i Disabled config.ini > ini.tmp
 for /f "delims=" %%x in (ini.tmp) do set Val=%%x
 if %val%==1 (goto disabled) ELSE (goto cmdst)
 :cmdst
-call rver.bat
+cd %cmddir%
+call r%startup%.bat
 goto :cmd
 :cmd
-cd C:\RMP\rycmd
-set "prmpt=RDRIVE"
+cd %cmddir%
 set "cmd="
 set /p"cmd=%prmpt%>"
 if not exist "r%cmd%.bat" (echo %cmd% is not a valid command or a file in %prmpt%.) ELSE call "r%cmd%.bat"
