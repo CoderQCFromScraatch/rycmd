aea4c162a8862284386c97537e3d3a360feb3b60
CoderQCFromScraatch <73763394+CoderQCFromScraatch@users.noreply.github.com>
2021-01-31 11:15:57 -0800

    Update rycmd.bat

--- rycmd.bat
+++ rycmd.bat
@@ -1,3 +1,8 @@
 @echo off
+cd C:\RMP\rycmd\RubCmSettings\
+call ini.bat /s RScript /i RequireAdmin config.ini>ini.tmp
+for /f "delims=" %%x in (ini.tmp) do set Vala=%%x
 title RyCMD
-call RConHost.exe
+cd C:\RMP\rycmd
+if %Vala%==1 (call host.exe) ELSE (call RConHost.exe)
+exit
